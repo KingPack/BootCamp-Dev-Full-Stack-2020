3f2a16eb9a76c1a26c8659670b78b4eabcff5062
KingPack <llhendrek@gmail.com>
2020-07-11 23:51:17 -0400

    add test.js

---
+++ modulo 01/cap 01/test.js
@@ -0,0 +1,30 @@
+let a = 5
+let b = 6
+
+if (a > b) {
+    console.log(a + ' e maior que ' + b)
+
+} else { 
+    if ( a < b){
+    console.log(a + 'é menor que ' + b)
+
+} else {
+    console.log(a + ' e igual a ' + b)
+    }
+} 
+
+let dia = 1
+
+if (dia === 1) {
+    console.log('Domingo')
+}
+else {
+    if (dia === 2){
+        console.log('segunda')
+}
+else {
+    if (dia === 3) {
+            consol.log('terça')
+        }
+    }
+}
