1a677da42d4142ff794385b3b62d65685b83f487
KingPack <llhendrek@gmail.com>
2020-07-13 19:20:35 -0400

    add funcoes.js :memo: adicionando arquivo e editando codigo

--- modulo 01/cap 01/test.js
+++ modulo 01/cap 01/test.js
@@ -44,7 +44,7 @@ else {
     }
 else {
     {
-    console.log('dia não existente "digite entre 1 a 7"')
+    console.log('dia não existente')
     }
      }
       }
@@ -54,5 +54,73 @@ else {
           }
            }
             
-        
+var r = 2
+// prettier-ignore
+switch(dia){
+    case 1: r = 'Domingo' ; 
+    break
+    case 2: r = 'Segunda' 
+    break
+    case 3: r = 'Terça'
+    break
+    case 4: r = 'Quarta'
+    break
+    case 5: r = 'Quinta'
+    break
+    case 6: r = "Sexta"
+    break
+    case 7: r = 'Sabado'
+    break
+    
+    default: r = 'Dia Inexistente'
+}
 
+console.log(r)
+
+// Operador Ternario
+
+a = 6
+b = 7
+
+if (a > b) {
+    console.log(a + ' é maior que ' + b)
+
+} else { 
+    if ( a < b){
+    console.log(a + ' é menor que ' + b)
+
+} else {
+    console.log(a + ' é igual a ' + b)
+    }
+}
+var resposta = a > b ? 'maior' : a < b ? 'menor' : 'igual'
+console.log(resposta)
+
+
+// somatorio com while
+
+let numeroAtual = 1 
+let somatorio = 0
+
+while (numeroAtual <= 10) {
+    //somatorio += numeroAtual
+    somatorio += numeroAtual
+    numeroAtual++
+}
+
+console.log('A soma é ' + somatorio)
+
+do {
+    somatorio += numeroAtual
+    numeroAtual++
+} while (numeroAtual <= 10)
+
+console.log('A soma é ' + somatorio)
+
+somatorio = 0
+
+for(numeroAtual = 1; numeroAtual <=10; numeroAtual++){
+    somatorio += numeroAtual
+}
+
+console.log('A soma é ' + somatorio)
---
+++ modulo 01/funcoes.js
@@ -0,0 +1,13 @@
+function sum(a, b){
+    return a + b
+}
+
+console.log(sum(1, 2))
+
+function compareNumbers(a, b){
+    return a > b ? 1 : a < b ? -1 : 0
+}
+
+console.log(compareNumbers(1, 1));
+console.log(compareNumbers(1, 2));
+console.log(compareNumbers(2, 1));
