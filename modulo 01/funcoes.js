d1f523e46482148b64531bdb376843dc8058c58a
KingPack <llhendrek@gmail.com>
2020-07-13 19:29:37 -0400

    :memo: add funcoes.jg adicionando funcoes.js e implementando codigo em test.js

--- modulo 01/funcoes.js
+++ modulo 01/funcoes.js
@@ -11,3 +11,15 @@ function compareNumbers(a, b){
 console.log(compareNumbers(1, 1));
 console.log(compareNumbers(1, 2));
 console.log(compareNumbers(2, 1));
+
+function SuperSum(from, upTo){
+    var sum = 0
+
+    for (var i = from; i <= upTo; i++){
+    }
+    return sum
+}
+
+console.log(SuperSum(1, 10))
+console.log(SuperSum(9, 100))
+console.log(SuperSum(200, 1000))
