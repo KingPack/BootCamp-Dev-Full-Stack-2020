1d9ed047691b0724e1df8b47cdd6a7538a0db62e
KingPack <llhendrek@gmail.com>
2021-03-11 22:35:24 -0400

    att

--- modulo 01/Desafio/js/scripts.js
+++ modulo 01/Desafio/js/scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', start)
 
-var globalNames = ['Um', 'Dois', 'Tres', 'Quatro'];
+var globalNames = ['Um', 'Dois', 'Tres', 'Quatro', 'Cinco'];
 var inputName = null;
 var isEditing = false;
 var currentIndex = true;
