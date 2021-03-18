2fc70cc2446de31ab5af177cd6e1919011dcc21b
KingPack <llhendrek@gmail.com>
2021-03-18 00:46:31 -0400

    att

--- modulo 01/Trabalho Pratico 1/js/script.js
+++ modulo 01/Trabalho Pratico 1/js/script.js
@@ -1,8 +1,26 @@
+let inputRangeRed = null
+let inputRangeGreen = null
+let inputRangeBlue = null
+
+let inputTextRed = null
+let inputTextGreen = null
+let inputTextBlue = null
+ 
+let divSquare = null
+
 window.addEventListener('load', () => {
     mapElements()
 })
 
 function mapElements() {
+    inputRangeRed = document.querySelector('#inputRangeRed')
+    inputRangeGreen = document.querySelector('#inputRangeGreen')
+    inputRangeBlue = document.querySelector('#inputRangeBlue')
 
+    inputTextRed = document.querySelector('#inputTexted')
+    inputTextGreen = document.querySelector('#inputTextGreen')
+    inputTextBlue = document.querySelector('#inputTextBlue')
 
+    divSquare = document.querySelector('#divSquare')
+    
 }
