28a5e071c2aae3a8309a4410743ae25bf613ed45
KingPack <llhendrek@gmail.com>
2021-03-18 14:17:21 -0400

    att

--- modulo 01/Trabalho Pratico 1/js/script.js
+++ modulo 01/Trabalho Pratico 1/js/script.js
@@ -8,8 +8,13 @@ let inputTextBlue = null
  
 let divSquare = null
 
+let r = '0'
+let g = '0'
+let b = '0'
+
 window.addEventListener('load', () => {
     mapElements()
+    syncDiv()
 })
 
 function mapElements() {
@@ -17,10 +22,34 @@ function mapElements() {
     inputRangeGreen = document.querySelector('#inputRangeGreen')
     inputRangeBlue = document.querySelector('#inputRangeBlue')
 
-    inputTextRed = document.querySelector('#inputTexted')
+    inputTextRed = document.querySelector('#inputTextRed')
     inputTextGreen = document.querySelector('#inputTextGreen')
     inputTextBlue = document.querySelector('#inputTextBlue')
 
     divSquare = document.querySelector('#divSquare')
+
+    inputRangeRed.addEventListener('change', handleInputRangeChange)
+    inputRangeGreen.addEventListener('change', handleInputRangeChange)
+    inputRangeBlue.addEventListener('change', handleInputRangeChange)    
+    
+}
+function handleInputRangeChange(event) {
+    const value = event.target.value
+    const id = event.target.id
+
+    switch (id) {
+        case "inputRangeRed": r = value; break
+        case "inputRangeGreen": g = value; break
+        case "inputRangeBlue": b = value; break
+    }
+    syncDiv();
+}
+
+function syncDiv(){
+    divSquare.style.backgroundColor = `rgb(${r},${g},${b})`
+
+    inputTextRed.value = r
+    inputTextGreen.value = g
+    inputTextBlue.value = b
     
 }
