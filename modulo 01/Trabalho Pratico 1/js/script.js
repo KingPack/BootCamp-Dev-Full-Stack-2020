0b8127c26b9daa9d56005142adf82e272a115855
KingPack <llhendrek@gmail.com>
2021-03-17 21:20:30 -0400

    att

--- modulo 01/Trabalho Pratico 1/js/script.js
+++ modulo 01/Trabalho Pratico 1/js/script.js
@@ -1,7 +1,13 @@
 console.log("Olá, Mundo");
 
-function rgb (){
-    var Red = inputRed.value
-   
-} 
-console.log(Red) 
+var Red = document.querySelector("#valueRed")
+var Green = document.querySelector("#valueGreen")
+var Blue = document.querySelector("#valueBlue")
+var rgb = document.querySelector("#displayRGB")
+var Display = document.querySelector("")
+function DisplayRGB () {
+
+
+}
+
+console.log(rgb.value) 
