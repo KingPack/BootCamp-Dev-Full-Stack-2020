cf7fd07c4b602efded693e4a5735b616eeb28a9c
KingPack <llhendrek@gmail.com>
2021-03-20 18:02:23 -0400

    att

--- 1. fundamentos/modulo 02/cap 01/script.js
+++ 1. fundamentos/modulo 02/cap 01/script.js
@@ -1,4 +1,4 @@
-'use stric'; 
+'use strict'; 
 
 function withVar() {
     for (var i = 0; i < 10; i++) {
@@ -8,4 +8,23 @@ function withVar() {
     console.log(i)
 }
 
+
+function withLet() {
+    for (let i = 0; i < 10; i++) {
+        console.log('let' + i)
+    }
+   // i = 20;
+    //console.log(i)
+}
+
 withVar()
+withLet()
+
+
+const c = 10
+c = 20
+
+const d = []
+console.log(d)
+
+d.push(1)
