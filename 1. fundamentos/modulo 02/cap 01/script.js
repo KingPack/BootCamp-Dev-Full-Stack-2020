a5f4be113410d2bf80c2415efb98b63440596a26
KingPack <llhendrek@gmail.com>
2021-03-20 18:26:35 -0400

    att

--- 1. fundamentos/modulo 02/cap 01/script.js
+++ 1. fundamentos/modulo 02/cap 01/script.js
@@ -21,10 +21,45 @@ withVar()
 withLet()
 
 
-const c = 10
-c = 20
+//const c = 10
+//c = 20
 
-const d = []
-console.log(d)
+//const d = []
+//console.log(d)
 
-d.push(1)
+//d.push(1)
+
+function sum (a, b) {
+    return a + b 
+}
+
+const sum2 = function(a, b) {
+    return a + b
+}
+
+const sum3 = (a, b) => {
+    return a + b
+}
+
+const sum4 = (a, b) => a + b
+
+console.log(sum(2,3))
+console.log(sum2(2,3))
+console.log(sum3(2,3))
+console.log(sum4(2,3))
+
+
+// template literals
+
+const nome = 'Lucas'
+const surName = 'Hendrek'
+const text1 = 'Meu nome e '+ nome + ' ' + surName
+const text2 = `Meu nome e ${nome} ${surName}`
+
+
+console.log(text1)
+console.log(text2)
+
+
+const sum5 = (a, b) => a + b
+console.log(sum5(2))
